fe028b58d632484f9bfa6b623f2c897cee91e328
egorsv1985 <egorvs2009@gmail.com>
2022-02-28 22:40:35 +0300

    added video fix bags background

--- src/js/app.js
+++ src/js/app.js
@@ -80,3 +80,33 @@ export function menuClose() {
 }
 
 menuInit();
+
+
+// video
+const video = document.querySelector(".c-video");
+const juice = document.querySelector(".orange-juice");
+const btn = document.getElementById("play-pause");
+
+function togglePlayPause() {
+  if (video.paused) {
+    btn.className = "pause";
+    video.play();
+  } else {
+    btn.className = "play";
+    video.pause();
+  }
+}
+
+video.onclick = function () {
+  togglePlayPause();
+};
+btn.onclick = function () {
+  togglePlayPause();
+};
+video.addEventListener("timeupdate", function () {
+  const juicePos = video.currentTime / video.duration;
+  juice.style.width = juicePos * 100 + "%";
+  if (video.ended) {
+    btn.className = "play";
+  }
+});
